fb384de9df0250f433a68e1cb792177e11c596f3
Greg <marshall_gj@hotmail.com>
2021-02-04 22:28:48 +0000

    Add probable switch to utility method.

--- src/main/java/com/marshall/customersubscriptionservice/web/util/MatchSummaryStringUtil.java
+++ src/main/java/com/marshall/customersubscriptionservice/web/util/MatchSummaryStringUtil.java
@@ -1,12 +1,26 @@
 package com.marshall.customersubscriptionservice.web.util;
 
 import com.marshall.customersubscriptionservice.model.event.Match;
+import com.marshall.customersubscriptionservice.web.constants.WebConstants;
 
 public class MatchSummaryStringUtil {
 
     public static String buildMatchSummary(Match match, String summaryType) {
+        String matchSummary = null;
 
+        switch (summaryType) {
+            case WebConstants.SUMMARY_TYPE_A_v_B:
+                // Todo build avb summary
+                matchSummary = "";
+                break;
+            case WebConstants.SUMMARY_TYPE_A_v_B_TIME:
+                // Todo build avb time summary
+                matchSummary = "";
+                break;
+            default:
+                throw new IllegalArgumentException();
+        }
 
-        return "";
+        return matchSummary;
     }
 }
