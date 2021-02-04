4555a2e57491d3aba2cd21f002876da1961daf4e
Greg <marshall_gj@hotmail.com>
2021-02-04 19:32:01 +0000

    Add a web constants class.

---
+++ src/main/java/com/marshall/customersubscriptionservice/web/constants/WebConstants.java
@@ -0,0 +1,7 @@
+package com.marshall.customersubscriptionservice.web.constants;
+
+public class WebConstants {
+
+    public static final String SUMMARY_TYPE_A_v_B = "AvB";
+    public static final String SUMMARY_TYPE_A_v_B_TIME = "AvBTime";
+}
