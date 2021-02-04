2349f706d8a933342e37e8e5345f379b41984729
Greg <marshall_gj@hotmail.com>
2021-02-04 18:47:26 +0000

    Add fields, constructor, getter/setters.

--- src/main/java/com/marshall/customersubscriptionservice/model/user/UserMatchLicence.java
+++ src/main/java/com/marshall/customersubscriptionservice/model/user/UserMatchLicence.java
@@ -1,4 +1,27 @@
 package com.marshall.customersubscriptionservice.model.user;
 
 public class UserMatchLicence {
+    private Long userId;
+    private Long matchId;
+
+    public UserMatchLicence(Long userId, Long matchId) {
+        this.userId = userId;
+        this.matchId = matchId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getMatchId() {
+        return matchId;
+    }
+
+    public void setMatchId(Long matchId) {
+        this.matchId = matchId;
+    }
 }
