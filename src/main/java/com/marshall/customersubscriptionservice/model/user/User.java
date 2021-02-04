fad8575f3c170c62e1ef1bf2cc051e40ea2207b2
Greg <marshall_gj@hotmail.com>
2021-02-04 18:06:26 +0000

    Add fields, constructor, getter/setters.

--- src/main/java/com/marshall/customersubscriptionservice/model/user/User.java
+++ src/main/java/com/marshall/customersubscriptionservice/model/user/User.java
@@ -1,4 +1,17 @@
 package com.marshall.customersubscriptionservice.model.user;
 
 public class User {
+    private Long userId;
+
+    public User(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
 }
