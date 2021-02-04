521ceb4d984653c98d7c6a9c41430dd5f4add201
Greg <marshall_gj@hotmail.com>
2021-02-04 18:47:10 +0000

    Implement skeletal first method.

--- src/main/java/com/marshall/customersubscriptionservice/service/UserLicenceServiceImpl.java
+++ src/main/java/com/marshall/customersubscriptionservice/service/UserLicenceServiceImpl.java
@@ -1,7 +1,15 @@
 package com.marshall.customersubscriptionservice.service;
 
+import com.marshall.customersubscriptionservice.model.event.Match;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserLicenceServiceImpl implements IUserLicenceService {
+
+    @Override
+    public List<Match> getLicencedMatches(Long userId) {
+        return null;
+    }
 }
