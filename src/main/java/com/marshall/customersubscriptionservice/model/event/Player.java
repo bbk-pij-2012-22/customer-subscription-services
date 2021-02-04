41d6830b5efc5edae3c6ec765d02a7566ad687f7
Greg <marshall_gj@hotmail.com>
2021-02-04 18:06:09 +0000

    Add constructor, getter/setters.

--- src/main/java/com/marshall/customersubscriptionservice/model/event/Player.java
+++ src/main/java/com/marshall/customersubscriptionservice/model/event/Player.java
@@ -3,4 +3,16 @@ package com.marshall.customersubscriptionservice.model.event;
 public class Player {
 
     private String playerName;
+
+    public Player(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
 }
