42bde86921f647a1732aa463374134a2153dafc8
Greg <marshall_gj@hotmail.com>
2021-02-04 18:05:48 +0000

    Add a method for fetching a user's licenced matches.

--- src/main/java/com/marshall/customersubscriptionservice/service/IUserLicenceService.java
+++ src/main/java/com/marshall/customersubscriptionservice/service/IUserLicenceService.java
@@ -1,4 +1,9 @@
 package com.marshall.customersubscriptionservice.service;
 
+import com.marshall.customersubscriptionservice.model.event.Match;
+import java.util.List;
+
 public interface IUserLicenceService {
+
+    List<Match> getLicencedMatches(Long userId);
 }
