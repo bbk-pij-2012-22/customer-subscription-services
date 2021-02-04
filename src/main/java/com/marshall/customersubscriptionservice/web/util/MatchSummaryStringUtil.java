7c74cb127c1bc50dc983356bf444937c55f72763
Greg <marshall_gj@hotmail.com>
2021-02-04 19:37:11 +0000

    Extract some summary specific logic out into its own utility class.

---
+++ src/main/java/com/marshall/customersubscriptionservice/web/util/MatchSummaryStringUtil.java
@@ -0,0 +1,12 @@
+package com.marshall.customersubscriptionservice.web.util;
+
+import com.marshall.customersubscriptionservice.model.event.Match;
+
+public class MatchSummaryStringUtil {
+
+    public static String buildMatchSummary(Match match) {
+
+
+        return "";
+    }
+}
