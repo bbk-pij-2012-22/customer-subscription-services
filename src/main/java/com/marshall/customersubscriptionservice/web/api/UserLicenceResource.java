0aab085e34cb3856bda0dbd17cc54e578097d193
Greg <marshall_gj@hotmail.com>
2021-02-04 18:46:39 +0000

    Implement the GET method for fetching matches licenced to the user. Add helper method to build the array of results.

--- src/main/java/com/marshall/customersubscriptionservice/web/api/UserLicenceResource.java
+++ src/main/java/com/marshall/customersubscriptionservice/web/api/UserLicenceResource.java
@@ -1,26 +1,51 @@
 package com.marshall.customersubscriptionservice.web.api;
 
+import com.marshall.customersubscriptionservice.model.event.Match;
 import com.marshall.customersubscriptionservice.service.IUserLicenceService;
-import com.marshall.customersubscriptionservice.web.model.UserLicenceResponse;
+import com.marshall.customersubscriptionservice.web.MatchSummaryStringUtil;
+import com.marshall.customersubscriptionservice.web.model.UserMatchLicenceWrapper;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
+
 
 @RestController
 @RequestMapping(path = "/api/user/licence")
 public class UserLicenceResource {
 
-    private IUserLicenceService userLicenceService;
+    private final IUserLicenceService userLicenceService;
 
     public UserLicenceResource(IUserLicenceService userLicenceService) {
-        Objects.requireNonNull(userLicenceService);
-        userLicenceService = userLicenceService;
+        this.userLicenceService = userLicenceService;
     }
-    public List<UserLicenceResponse> getUserMatchLicences() {
 
-        return Collections.emptyList(); // gm obv remove when implemented
+    @GetMapping(path = "/{userId}/matches", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<UserMatchLicenceWrapper> getUserMatchLicences(@PathVariable("userId") Long userId,
+                                                              @RequestParam String summaryType) {
+        List<Match> licencedMatches = userLicenceService.getLicencedMatches(userId);
+
+        return buildResponse(licencedMatches, summaryType);
+    }
+
+    private List<UserMatchLicenceWrapper> buildResponse(List<Match> licencedMatches, String summaryType) {
+
+        List<UserMatchLicenceWrapper> response = new ArrayList<>();
+        for (Match match : licencedMatches) {
+            UserMatchLicenceWrapper userLicenceWrapper = new UserMatchLicenceWrapper(match);
+
+            if (summaryType != null) {
+               userLicenceWrapper.setSummary(MatchSummaryStringUtil.buildMatchSummary(match));
+            }
+
+            response.add(userLicenceWrapper);
+        }
+
+        return response;
     }
 }
