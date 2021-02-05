bfd5ece232c4b07d5972fd9bd1fad90d5f2b8adf
Greg <marshall_gj@hotmail.com>
2021-02-05 15:34:03 +0000

    Turn this model into a hibernate entity.

--- src/main/java/com/marshall/customersubscriptionservice/model/event/Player.java
+++ src/main/java/com/marshall/customersubscriptionservice/model/event/Player.java
@@ -1,8 +1,21 @@
 package com.marshall.customersubscriptionservice.model.event;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "player")
 public class Player {
 
+    @Id
+    @Column(name = "player_id")
+    private Long playerId;
     private String playerName;
+
+    public Player() {
+    }
 
     public Player(String playerName) {
         this.playerName = playerName;
@@ -14,5 +27,13 @@ public class Player {
 
     public void setPlayerName(String playerName) {
         this.playerName = playerName;
+    }
+
+    public Long getPlayerId() {
+        return playerId;
+    }
+
+    public void setPlayerId(Long playerId) {
+        this.playerId = playerId;
     }
 }
