fb5ed97f0cd179b7a81b52f6876dfd3f988f5379
Greg <marshall_gj@hotmail.com>
2021-02-05 15:36:59 +0000

    Turn this model into a hibernate entity.

--- src/main/java/com/marshall/customersubscriptionservice/model/user/UserTournamentLicence.java
+++ src/main/java/com/marshall/customersubscriptionservice/model/user/UserTournamentLicence.java
@@ -1,13 +1,27 @@
 package com.marshall.customersubscriptionservice.model.user;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user_tournament_licence")
 public class UserTournamentLicence {
 
+    @Id
+    private Long userTournamentLicenceId;
     private Long userId;
     private Long tournamentId;
 
-    public UserTournamentLicence(Long userId, Long tournamentId) {
-        this.userId = userId;
-        this.tournamentId = tournamentId;
+    public UserTournamentLicence() {
+    }
+
+    public Long getUserTournamentLicenceId() {
+        return userTournamentLicenceId;
+    }
+
+    public void setUserTournamentLicenceId(Long userTournamentLicenceId) {
+        this.userTournamentLicenceId = userTournamentLicenceId;
     }
 
     public Long getUserId() {
