a38b829d10074bb6682a54c83c2f017db16795c4
Greg <marshall_gj@hotmail.com>
2021-02-05 21:18:43 +0000

    Add some user validation.

--- src/main/java/com/marshall/customersubscriptionservice/service/UserLicenceServiceImpl.java
+++ src/main/java/com/marshall/customersubscriptionservice/service/UserLicenceServiceImpl.java
@@ -1,6 +1,9 @@
 package com.marshall.customersubscriptionservice.service;
 
 import com.marshall.customersubscriptionservice.dao.IUserMatchLicenceRepository;
+import com.marshall.customersubscriptionservice.dao.IUserRepository;
+import com.marshall.customersubscriptionservice.exception.UserNotExistsException;
+import com.marshall.customersubscriptionservice.model.user.User;
 import com.marshall.customersubscriptionservice.model.user.UserMatchLicence;
 import org.springframework.stereotype.Service;
 
@@ -10,13 +13,24 @@ import java.util.List;
 public class UserLicenceServiceImpl implements IUserLicenceService {
 
     private final IUserMatchLicenceRepository userMatchLicenceRepository;
+    private final IUserRepository userRepository;
 
-    public UserLicenceServiceImpl(IUserMatchLicenceRepository userMatchLicenceRepository) {
+    public UserLicenceServiceImpl(IUserMatchLicenceRepository userMatchLicenceRepository,
+                                  IUserRepository userRepository) {
         this.userMatchLicenceRepository = userMatchLicenceRepository;
+        this.userRepository = userRepository;
     }
 
     @Override
-    public List<UserMatchLicence> getLicencedMatches(Long userId) {
+    public List<UserMatchLicence> getLicencedMatches(Long userId) throws UserNotExistsException {
+        validateUserId(userId);
         return userMatchLicenceRepository.findByUserId(userId);
+    }
+
+    private void validateUserId(Long userId) throws UserNotExistsException {
+        User user = userRepository.findByUserId(userId);
+        if (user == null) {
+            throw new UserNotExistsException("User ID " + userId + " does not exist.");
+        }
     }
 }
