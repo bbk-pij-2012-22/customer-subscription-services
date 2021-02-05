da4eaf798ad59ddff3e00416d27ea688da2080a3
Greg <marshall_gj@hotmail.com>
2021-02-05 21:16:47 +0000

    Add an exception handling class so we can just return suitable messages to the client when we throw custom exceptions from deeper in the code.

---
+++ src/main/java/com/marshall/customersubscriptionservice/web/exception/handling/ExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.marshall.customersubscriptionservice.web.exception.handling;
+
+import com.marshall.customersubscriptionservice.exception.UserNotExistsException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(value = UserNotExistsException.class)
+    protected ResponseEntity<Object> handleException(UserNotExistsException e, WebRequest request) {
+        String responseBody = e.getMessage();
+        return handleExceptionInternal(e, responseBody, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    }
+}
