048a2a1ca2d0c7717aa326eb04f717a103096864
Greg <marshall_gj@hotmail.com>
2021-02-05 19:10:12 +0000

    Extend integration testing super class. Introduce a blank test that hits the DB.

--- src/test/java/com/marshall/customersubscriptionservice/service/UserLicenceServiceImplTest.java
+++ src/test/java/com/marshall/customersubscriptionservice/service/UserLicenceServiceImplTest.java
@@ -1,5 +1,28 @@
 package com.marshall.customersubscriptionservice.service;
 
-class UserLicenceServiceImplTest {
+import com.marshall.customersubscriptionservice.IntegrationTest;
+import com.marshall.customersubscriptionservice.model.user.UserMatchLicence;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
 
+import javax.transaction.Transactional;
+import java.util.List;
+
+class UserLicenceServiceImplTest extends IntegrationTest {
+
+    @Autowired
+    private IUserLicenceService userLicenceService;
+
+/*    public UserLicenceServiceImplTest(IUserLicenceService userLicenceService) {
+        this.userLicenceService = userLicenceService;
+    }*/
+
+    @Test
+    @Rollback
+    @Transactional
+    public void fetchUserMatchLicences() {
+        List<UserMatchLicence> userMatchLicences = userLicenceService.getLicencedMatches(1L);
+        //todo: assertions
+    }
 }
