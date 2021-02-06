1f33ad9c9fa30ac3340004b0eec161b58cc85dbb
gmarshall <marshall_gj@hotmail.com>
2021-02-06 10:12:19 +0000

    Add a test to assess avb string building.

--- src/test/java/com/marshall/customersubscriptionservice/web/util/MatchSummaryStringUtilTest.java
+++ src/test/java/com/marshall/customersubscriptionservice/web/util/MatchSummaryStringUtilTest.java
@@ -3,10 +3,12 @@ package com.marshall.customersubscriptionservice.web.util;
 import com.marshall.customersubscriptionservice.model.event.Match;
 import com.marshall.customersubscriptionservice.model.event.Player;
 import com.marshall.customersubscriptionservice.model.user.UserMatchLicence;
+import com.marshall.customersubscriptionservice.web.constants.WebConstants;
 import org.junit.jupiter.api.Test;
 
 import java.util.Date;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class MatchSummaryStringUtilTest {
@@ -15,9 +17,12 @@ class MatchSummaryStringUtilTest {
     private Match match;
 
     public MatchSummaryStringUtilTest() {
+        Date now = new Date();
         Player playerA = new Player();
+        playerA.setPlayerName("A.Agassi");
         Player playerB = new Player();
-        match = new Match(1L, 1L, new Date(), playerA, playerB);
+        playerB.setPlayerName("J.McEnroe");
+        match = new Match(1L, 1L, now, playerA, playerB);
         userMatchLicence = new UserMatchLicence(1L, 1L, match);
     }
 
@@ -25,5 +30,12 @@ class MatchSummaryStringUtilTest {
     public void buildMatchSummaryIllegalArgumentTest() {
         assertThrows(IllegalArgumentException.class, () ->
         MatchSummaryStringUtil.buildMatchSummary(userMatchLicence, "illegal argument"));
+    }
+
+    @Test
+    public void buildMatchSummaryAvBTest() {
+        String expected = "A.Agassi vs J.McEnroe";
+        String actual = MatchSummaryStringUtil.buildMatchSummary(userMatchLicence, WebConstants.SUMMARY_TYPE_A_v_B);
+        assertEquals(expected, actual);
     }
 }
