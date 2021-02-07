5d5c9fde19b15b3b56fb6fcaab2ec2b2bb6022d5
Greg <marshall_gj@hotmail.com>
2021-02-07 18:44:50 +0000

    Add new unit tests for past/future match summaries.

--- src/test/java/com/marshall/customersubscriptionservice/web/util/MatchSummaryStringUtilTest.java
+++ src/test/java/com/marshall/customersubscriptionservice/web/util/MatchSummaryStringUtilTest.java
@@ -4,6 +4,7 @@ import com.marshall.customersubscriptionservice.model.event.Match;
 import com.marshall.customersubscriptionservice.model.event.Player;
 import com.marshall.customersubscriptionservice.model.customer.CustomerMatchLicence;
 import com.marshall.customersubscriptionservice.web.constants.WebConstants;
+import org.apache.commons.lang.time.DateUtils;
 import org.junit.jupiter.api.Test;
 
 import java.util.Date;
@@ -36,6 +37,29 @@ class MatchSummaryStringUtilTest {
     public void buildMatchSummaryAvBTest() {
         String expected = "A.Agassi vs J.McEnroe";
         String actual = MatchSummaryStringUtil.buildMatchSummary(customerMatchLicence, WebConstants.SUMMARY_TYPE_A_v_B);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void buildMatchSummaryAvBTimeFutureTest() {
+        Date originalStartDate = customerMatchLicence.getMatch().getStartDate();
+        Date fortyMinutesLater = DateUtils.addMinutes(originalStartDate,40);
+        customerMatchLicence.getMatch().setStartDate(fortyMinutesLater);
+
+        //milliseconds run time mean it'll be just < 40 minutes when the assertion is done :)
+        String expected = "A.Agassi vs J.McEnroe, starts in 39 minutes";
+        String actual = MatchSummaryStringUtil.buildMatchSummary(customerMatchLicence, WebConstants.SUMMARY_TYPE_A_v_B_TIME);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void buildMatchSummaryAvBTimePastTest() {
+        Date originalStartDate = customerMatchLicence.getMatch().getStartDate();
+        Date fortyMinutesLater = DateUtils.addMinutes(originalStartDate,-40);
+        customerMatchLicence.getMatch().setStartDate(fortyMinutesLater);
+
+        String expected = "A.Agassi vs J.McEnroe, started 40 minutes ago";
+        String actual = MatchSummaryStringUtil.buildMatchSummary(customerMatchLicence, WebConstants.SUMMARY_TYPE_A_v_B_TIME);
         assertEquals(expected, actual);
     }
 }
